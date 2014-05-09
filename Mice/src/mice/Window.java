eabbd126e25980979550bb608e84332eb3daf2a6
pjaM1 <payam.khatibnia@gmail.com>
2014-05-09 11:48:29 +0200

    Update Window.java

--- Mice/src/mice/Window.java
+++ Mice/src/mice/Window.java
@@ -77,7 +77,7 @@ public class Window extends javax.swing.JFrame {
 
     private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
         // TODO add your handling code here:
-        JOptionPane.showMessageDialog(this, "Hello");
+        JOptionPane.showMessageDialog(this, "Hello from the web");
             
     }//GEN-LAST:event_jButton1MouseClicked
 
